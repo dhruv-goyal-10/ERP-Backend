34b41349d83a61b0b5816527c6b456e5891ad9a2
anshumannandan <anshuman21xxx034@akgec.ac.in>
2022-10-27 17:37:22 +0530

    dotenv

--- ERP_backend/settings.py
+++ ERP_backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a991g3f5&)+^3d!t+bw7ri4ik!4pls63j@pcp7bh#8uj4d-_yw'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'edumate.erp@gmail.com'
-EMAIL_HOST_PASSWORD = 'mrzobrqsxrndpwjy'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
