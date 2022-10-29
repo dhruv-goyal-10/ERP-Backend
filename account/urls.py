841edddcf2c005e0cb771f927f69bd2798f4f736
anshumannandan <anshuman21xxx034@akgec.ac.in>
2022-10-30 02:16:11 +0530

    api to add teachers

--- account/urls.py
+++ account/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('sendotp/',SendOTPView.as_view(), name='sendotp'),
     path('verifyotp/',VerifyOTPView.as_view(), name='verifyotp'),
     path('changepassword/',ChangePasswordView.as_view(), name='changepassword'),
-    path('addstudent/',AddStudent.as_view(), name='addStudent')
+    path('addstudent/',AddStudent.as_view(), name='addStudent'),
+    path('addteacher/',AddTeacher.as_view(), name='addTeacher')
 ]
