f418b7942a344e4f208cf331458a0db11a885fda
anshumannandan <anshuman21xxx034@akgec.ac.in>
2022-11-05 11:29:08 +0530

    cors allow credentials

--- ERP_backend/settings.py
+++ ERP_backend/settings.py
@@ -197,6 +197,8 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS=True
+CORS_ALLOW_CREDENTIALS = True
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
